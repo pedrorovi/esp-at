86ade8eb974d3b5eb81d67802a1de711c99105e2
Pedro Rodríguez Vieites <p.rodriguez@scrobotics.es>
2024-07-03 10:58:44 +0200

    feat: Update esp32-c6 version to v4.0.0.0 and core version to 3fe3806c

--- components/at/src/at_eth_init.c
+++ components/at/src/at_eth_init.c
@@ -1,62 +1,246 @@
-/*
- * ESPRESSIF MIT License
- *
- * Copyright (c) 2017 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
- *
- * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP32 only, in which case,
- * it is free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
-#include "esp_netif.h"
-
-#ifdef CONFIG_AT_ETHERNET_SUPPORT
-#include "string.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
 #include "esp_eth.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include <stdio.h>
+#include <string.h>
 
-#define CONFIG_AT_ETH_MDC_GPIO         23
-#define CONFIG_AT_ETH_MDIO_GPIO        18
-#define CONFIG_AT_ETH_PHY_RST_GPIO     5
-#define CONFIG_AT_ETH_PHY_ADDR         1
+#if CONFIG_ETH_USE_SPI_ETHERNET
+#include "driver/spi_master.h"
+#endif // CONFIG_ETH_USE_SPI_ETHERNET
 
-bool esp_at_get_eth_default_config(esp_eth_config_t* config)
-{
-    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
-    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+static const char *TAG = "eth_example";
 
-    phy_config.phy_addr = CONFIG_AT_ETH_PHY_ADDR;
-    phy_config.reset_gpio_num = CONFIG_AT_ETH_PHY_RST_GPIO;
-    eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG();
-    esp32_emac_config.smi_mdc_gpio_num = CONFIG_AT_ETH_MDC_GPIO;
-    esp32_emac_config.smi_mdio_gpio_num = CONFIG_AT_ETH_MDIO_GPIO;
-    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
+#define CONFIG_AT_ETH_MDC_GPIO 23
+#define CONFIG_AT_ETH_MDIO_GPIO 18
+#define CONFIG_AT_ETH_PHY_RST_GPIO 5
+#define CONFIG_AT_ETH_PHY_ADDR 1
 
-#if CONFIG_PHY_IP101
-    esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phy_config);
-#elif CONFIG_PHY_RTL8201
-    esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
-#elif CONFIG_PHY_LAN8720
-    esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
-#elif CONFIG_PHY_DP83848
-    esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
+#if CONFIG_ETH_USE_SPI_ETHERNET
+#define INIT_SPI_ETH_MODULE_CONFIG(eth_module_config, num)                     \
+  do {                                                                         \
+    eth_module_config[num].spi_cs_gpio = CONFIG_ETH_SPI_CS##num##_GPIO;        \
+    eth_module_config[num].int_gpio = CONFIG_ETH_SPI_INT##num##_GPIO;          \
+    eth_module_config[num].phy_reset_gpio =                                    \
+        CONFIG_ETH_SPI_PHY_RST##num##_GPIO;                                    \
+    eth_module_config[num].phy_addr = CONFIG_ETH_SPI_PHY_ADDR##num;            \
+  } while (0)
+
+typedef struct {
+  uint8_t spi_cs_gpio;
+  uint8_t int_gpio;
+  int8_t phy_reset_gpio;
+  uint8_t phy_addr;
+} spi_eth_module_config_t;
 #endif
 
-    esp_eth_config_t eth_config = ETH_DEFAULT_CONFIG(mac, phy);
-    memcpy(config, &eth_config, sizeof(esp_eth_config_t));
-    
-    return true;
+/** Event handler for Ethernet events */
+static void eth_event_handler(void *arg, esp_event_base_t event_base,
+                              int32_t event_id, void *event_data) {
+  uint8_t mac_addr[6] = {0};
+  /* we can get the ethernet driver handle from event data */
+  esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
+
+  switch (event_id) {
+  case ETHERNET_EVENT_CONNECTED:
+    esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
+    ESP_LOGI(TAG, "Ethernet Link Up");
+    ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x", mac_addr[0],
+             mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+    break;
+  case ETHERNET_EVENT_DISCONNECTED:
+    ESP_LOGI(TAG, "Ethernet Link Down");
+    break;
+  case ETHERNET_EVENT_START:
+    ESP_LOGI(TAG, "Ethernet Started");
+    break;
+  case ETHERNET_EVENT_STOP:
+    ESP_LOGI(TAG, "Ethernet Stopped");
+    break;
+  default:
+    break;
+  }
 }
+
+/** Event handler for IP_EVENT_ETH_GOT_IP */
+static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
+                                 int32_t event_id, void *event_data) {
+  ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+  const esp_netif_ip_info_t *ip_info = &event->ip_info;
+
+  ESP_LOGI(TAG, "Ethernet Got IP Address");
+  ESP_LOGI(TAG, "~~~~~~~~~~~");
+  ESP_LOGI(TAG, "ETHIP:" IPSTR, IP2STR(&ip_info->ip));
+  ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
+  ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
+  ESP_LOGI(TAG, "~~~~~~~~~~~");
+}
+
+bool esp_at_get_eth_default_config() {
+  // Initialize TCP/IP network interface (should be called only once in
+  // application)
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  // Create default event loop that running in background
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+#if CONFIG_ETH_USE_SPI_ETHERNET
+  // Create instance(s) of esp-netif for SPI Ethernet(s)
+  esp_netif_inherent_config_t esp_netif_config =
+      ESP_NETIF_INHERENT_DEFAULT_ETH();
+  esp_netif_config_t cfg_spi = {.base = &esp_netif_config,
+                                .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH};
+  esp_netif_t *eth_netif_spi[CONFIG_SPI_ETHERNETS_NUM] = {NULL};
+  char if_key_str[10];
+  char if_desc_str[10];
+  char num_str[3];
+  for (int i = 0; i < CONFIG_SPI_ETHERNETS_NUM; i++) {
+    itoa(i, num_str, 10);
+    strcat(strcpy(if_key_str, "ETH_SPI_"), num_str);
+    strcat(strcpy(if_desc_str, "eth"), num_str);
+    esp_netif_config.if_key = if_key_str;
+    esp_netif_config.if_desc = if_desc_str;
+    esp_netif_config.route_prio = 30 - i;
+    eth_netif_spi[i] = esp_netif_new(&cfg_spi);
+  }
+
+  // Init MAC and PHY configs to default
+  eth_mac_config_t mac_config_spi = ETH_MAC_DEFAULT_CONFIG();
+  eth_phy_config_t phy_config_spi = ETH_PHY_DEFAULT_CONFIG();
+
+  // Install GPIO ISR handler to be able to service SPI Eth modlues interrupts
+  gpio_install_isr_service(0);
+
+  // Init SPI bus
+  spi_bus_config_t buscfg = {
+      .miso_io_num = CONFIG_ETH_SPI_MISO_GPIO,
+      .mosi_io_num = CONFIG_ETH_SPI_MOSI_GPIO,
+      .sclk_io_num = CONFIG_ETH_SPI_SCLK_GPIO,
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+  };
+  ESP_ERROR_CHECK(
+      spi_bus_initialize(CONFIG_ETH_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+  // Init specific SPI Ethernet module configuration from Kconfig (CS GPIO,
+  // Interrupt GPIO, etc.)
+  spi_eth_module_config_t spi_eth_module_config[CONFIG_SPI_ETHERNETS_NUM];
+  INIT_SPI_ETH_MODULE_CONFIG(spi_eth_module_config, 0);
+#if CONFIG_EXAMPLE_SPI_ETHERNETS_NUM > 1
+  INIT_SPI_ETH_MODULE_CONFIG(spi_eth_module_config, 1);
 #endif
+
+  // Configure SPI interface and Ethernet driver for specific SPI module
+  esp_eth_mac_t *mac_spi[CONFIG_SPI_ETHERNETS_NUM];
+  esp_eth_phy_t *phy_spi[CONFIG_SPI_ETHERNETS_NUM];
+  esp_eth_handle_t eth_handle_spi[CONFIG_SPI_ETHERNETS_NUM] = {NULL};
+  spi_device_interface_config_t spi_devcfg = {
+      .mode = 0,
+      .clock_speed_hz = CONFIG_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
+      .queue_size = 20};
+  for (int i = 0; i < CONFIG_SPI_ETHERNETS_NUM; i++) {
+    // Set SPI module Chip Select GPIO
+    spi_devcfg.spics_io_num = spi_eth_module_config[i].spi_cs_gpio;
+    // Set remaining GPIO numbers and configuration used by the SPI module
+    phy_config_spi.phy_addr = spi_eth_module_config[i].phy_addr;
+    phy_config_spi.reset_gpio_num = spi_eth_module_config[i].phy_reset_gpio;
+#if CONFIG_ETH_SPI_ETHERNET_KSZ8851SNL
+    eth_ksz8851snl_config_t ksz8851snl_config =
+        ETH_KSZ8851SNL_DEFAULT_CONFIG(CONFIG_ETH_SPI_HOST, &spi_devcfg);
+    ksz8851snl_config.int_gpio_num = spi_eth_module_config[i].int_gpio;
+    mac_spi[i] =
+        esp_eth_mac_new_ksz8851snl(&ksz8851snl_config, &mac_config_spi);
+    phy_spi[i] = esp_eth_phy_new_ksz8851snl(&phy_config_spi);
+#elif CONFIG_ETH_SPI_ETHERNET_DM9051
+    eth_dm9051_config_t dm9051_config =
+        ETH_DM9051_DEFAULT_CONFIG(CONFIG_ETH_SPI_HOST, &spi_devcfg);
+    dm9051_config.int_gpio_num = spi_eth_module_config[i].int_gpio;
+    mac_spi[i] = esp_eth_mac_new_dm9051(&dm9051_config, &mac_config_spi);
+    phy_spi[i] = esp_eth_phy_new_dm9051(&phy_config_spi);
+#elif CONFIG_ETH_SPI_ETHERNET_W5500
+    ESP_LOGI(TAG, "CONFIG_ETH_SPI_ETHERNET_W5500");
+    eth_w5500_config_t w5500_config =
+        ETH_W5500_DEFAULT_CONFIG(CONFIG_ETH_SPI_HOST, &spi_devcfg);
+    w5500_config.int_gpio_num = spi_eth_module_config[i].int_gpio;
+    mac_spi[i] = esp_eth_mac_new_w5500(&w5500_config, &mac_config_spi);
+    // ESP_LOGI(TAG, "Mac is null? %d", mac_spi[i] == NULL);
+    phy_spi[i] = esp_eth_phy_new_w5500(&phy_config_spi);
+    // ESP_LOGI(TAG, "Phy is null? %d", phy_spi[i] == NULL);
+#endif
+    esp_eth_config_t eth_config_spi =
+        ETH_DEFAULT_CONFIG(mac_spi[i], phy_spi[i]);
+    ESP_ERROR_CHECK(
+        esp_eth_driver_install(&eth_config_spi, &eth_handle_spi[i]));
+
+    /* The SPI Ethernet module might not have a burned factory MAC address, we
+   cat to set it manually. 02:00:00 is a Locally Administered OUI range so
+   should not be used except when testing on a LAN under your control.
+    */
+    ESP_ERROR_CHECK(
+        esp_eth_ioctl(eth_handle_spi[i], ETH_CMD_S_MAC_ADDR,
+                      (uint8_t[]){0x02, 0x00, 0x00, 0x12, 0x34, 0x56 + i}));
+
+    // attach Ethernet driver to TCP/IP stack
+    ESP_ERROR_CHECK(esp_netif_attach(
+        eth_netif_spi[i], esp_eth_new_netif_glue(eth_handle_spi[i])));
+  }
+
+#else
+
+  // Create new default instance of esp-netif for Ethernet
+  esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+  esp_netif_t *eth_netif = esp_netif_new(&cfg);
+
+  // Init MAC and PHY configs to default
+  eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+  eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+
+  phy_config.phy_addr = CONFIG_AT_ETH_PHY_ADDR;
+  phy_config.reset_gpio_num = CONFIG_AT_ETH_PHY_RST_GPIO;
+  eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG();
+  esp32_emac_config.smi_mdc_gpio_num = CONFIG_AT_ETH_MDC_GPIO;
+  esp32_emac_config.smi_mdio_gpio_num = CONFIG_AT_ETH_MDIO_GPIO;
+  esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
+#if CONFIG_PHY_IP101
+  esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phy_config);
+#elif CONFIG_PHY_RTL8201
+  esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
+#elif CONFIG_PHY_LAN8720
+  esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
+#elif CONFIG_PHY_DP83848
+  esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
+#else
+  esp_eth_phy_t *phy = NULL;
+#endif
+  esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
+  esp_eth_handle_t eth_handle = NULL;
+  // ESP_ERROR_CHECK(esp_eth_driver_install(&config, &eth_handle));
+  /* attach Ethernet driver to TCP/IP stack */
+  ESP_ERROR_CHECK(
+      esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
+
+#endif // CONFIG_ETH_USE_SPI_ETHERNET
+
+  // Register user defined event handers
+  ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID,
+                                             &eth_event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP,
+                                             &got_ip_event_handler, NULL));
+
+  /* start Ethernet driver state machine */
+
+#if CONFIG_ETH_USE_SPI_ETHERNET
+  for (int i = 0; i < CONFIG_SPI_ETHERNETS_NUM; i++) {
+    ESP_ERROR_CHECK(esp_eth_start(eth_handle_spi[i]));
+  }
+#else  // CONFIG_ETH_USE_SPI_ETHERNET
+  ESP_ERROR_CHECK(esp_eth_start(eth_handle));
+#endif // CONFIG_ETH_USE_ESP32_EMAC
+
+  return true;
+}
